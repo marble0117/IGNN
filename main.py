142cdeffc867684c2d05b2813aaa08344b87365f
marble0117 <guitarmarble0117@gmail.com>
2019-09-03 00:14:43 +0900

    add t-SNE visualization

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-from dgl.data import citation_graph as citegrh
-from torch_geometric.datasets import Planetoid
+import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
+from dgl.data import citation_graph as citegrh
+from sklearn.manifold import TSNE
+from torch_geometric.datasets import Planetoid
+from torch_geometric.utils import add_self_loops
 
 from allsumSVC import *
 from allsumSLP import *
 from learnProp import *
+from improvedGCN import *
+
+def draw_nx(graph, labels):
+    G = nx.from_edgelist(np.array(graph).T)
+    y = list(labels)
+    color = []
+    for v in list(G):
+        color.append(y[v])
+    pos = nx.spring_layout(G)
+    nx.draw_networkx(G, pos, k=1, node_color=color)
+    plt.show()
+
+def draw_tsne(graph, E, labels):
+    graph = add_self_loops(graph)[0]
+    edges = list(graph)
+    weight = list(E.detach().numpy())
+    edgelist = []
+    for i, w in enumerate(weight):
+        edgelist.append((int(edges[0][i]), int(edges[1][i]), float(w)))
+    G = nx.Graph()
+    G.add_weighted_edges_from(edgelist)
+
+    G = nx.from_edgelist(np.array(graph).T)
+    A = nx.to_numpy_matrix(G)
+    y = list(labels)
+    color = []
+    for v in list(G):
+        color.append(y[v])
+    X_reduced = TSNE(n_components=2, random_state=0, n_iter=2000).fit_transform(A, y=color)
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=color)
+    plt.show()
 
 if __name__ == "__main__":
     # data = citegrh.load_cora()
@@ -19,8 +54,13 @@ if __name__ == "__main__":
     train_mask = data.train_mask
     val_mask = data.val_mask
     test_mask = data.test_mask
-    lam1 = 1e-4
+    lam1 = 0
 
     # svc_experiment(graph, features, labels, train_mask, test_mask, 3)
     # neural_experiment(graph, features, labels, train_mask, test_mask, 3)
-    learnProp_experiment(graph, features, labels, train_mask, val_mask, test_mask, lam1)
+    E = learnProp_experiment(graph, features, labels, train_mask, val_mask, test_mask, lam1)
+    # improvedGCN(graph, features, labels, train_mask, val_mask, test_mask)
+
+    # draw_nx(graph, labels)
+    draw_tsne(graph, E, labels)
+
