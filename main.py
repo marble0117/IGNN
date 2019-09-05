661a31c1e8b9d56faec7759ab9c5721a168ff363
marble0117 <guitarmarble0117@gmail.com>
2019-09-05 10:11:07 +0900

    delete t-SNE

--- main.py
+++ main.py
@@ -30,38 +30,18 @@ def draw_nx(graph, E, labels):
     for v in list(G):
         color.append(y[v])
 
-    pos = nx.spring_layout(G)
+    pos = nx.spring_layout(G, k=0.1)
     nx.draw_networkx_nodes(G, pos, node_color=color, node_size=50)
     # edge importance < 0.5
-    nx.draw_networkx_edges(G, pos, edgelist=weakedge, edge_color='red')
+    nx.draw_networkx_edges(G, pos, edgelist=weakedge, edge_color='red', width=2.0)
     # edge importance > 0.5
-    nx.draw_networkx_edges(G, pos, edgelist=strongedge, edge_color='black', alpha=0.2)
-    plt.show()
-
-def draw_tsne(graph, E, labels):
-    graph = add_self_loops(graph)[0]
-    edges = list(graph)
-    weight = list(E.detach().numpy())
-    edgelist = []
-    for i, w in enumerate(weight):
-        edgelist.append((int(edges[0][i]), int(edges[1][i]), float(w)))
-    G = nx.Graph()
-    G.add_weighted_edges_from(edgelist)
-
-    G = nx.from_edgelist(np.array(graph).T)
-    A = nx.to_numpy_matrix(G)
-    y = list(labels)
-    color = []
-    for v in list(G):
-        color.append(y[v])
-    X_reduced = TSNE(n_components=2, random_state=0, n_iter=2000).fit_transform(A, y=color)
-    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=color)
+    nx.draw_networkx_edges(G, pos, edgelist=strongedge, edge_color='black', width=0.5, alpha=0.1)
     plt.show()
 
 if __name__ == "__main__":
-    # dataset = Planetoid(root='/tmp/Cora', name='Cora')
+    dataset = Planetoid(root='/tmp/Cora', name='Cora')
     # dataset = Planetoid(root='/tmp/Pubmed', name="Pubmed")
-    dataset = Planetoid(root='/tmp/Citeseer', name='Citeseer')
+    # dataset = Planetoid(root='/tmp/Citeseer', name='Citeseer')
     data = dataset[0]
     features = data.x
     labels = data.y
@@ -77,5 +57,3 @@ if __name__ == "__main__":
     # improvedGCN(graph, features, labels, train_mask, val_mask, test_mask)
 
     draw_nx(graph, E, labels)
-    # draw_tsne(graph, E, labels)
-
