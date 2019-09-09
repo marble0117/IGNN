d9bec3aee9f8bc39d36e8e77419d6a856f3c22a4
marble0117 <guitarmarble0117@gmail.com>
2019-09-09 22:03:04 +0900

    change the drawing function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
+import torch
 from torch_geometric.datasets import Planetoid
 from torch_geometric.utils import add_self_loops
 
@@ -10,6 +11,7 @@ from allsumSLP import *
 from learnProp import *
 from improvedGCN import *
 from models import *
+
 
 def draw_nx(graph, E, labels):
     graph = add_self_loops(graph)[0]
@@ -31,17 +33,29 @@ def draw_nx(graph, E, labels):
     for v in list(G):
         color.append(y[v])
 
-    pos = nx.spring_layout(G, k=0.1)
-    nx.draw_networkx_nodes(G, pos, node_color=color, node_size=50)
+    train_node = torch.where(train_mask == 1)[0].tolist()
+    pos = nx.spring_layout(G, k=0.04, weight=None)
+    # nodes
+    # set the size of training nodes to 90
+    node_size = [10] * G.number_of_nodes()
+    for v in list(G):
+        if v in train_node:
+            node_size[v] = 90
+    nx.draw_networkx_nodes(G, pos, node_color=color, node_size=node_size)
     # edge importance < 0.5
     nx.draw_networkx_edges(G, pos, edgelist=weakedge, edge_color='red', width=2.0)
     # edge importance > 0.5
-    nx.draw_networkx_edges(G, pos, edgelist=strongedge, edge_color='black', width=0.5, alpha=0.1)
+    nx.draw_networkx_edges(G, pos, edgelist=strongedge, edge_color='black', width=0.3, alpha=0.1)
     plt.show()
 
+    # historgram
+    plt.hist(E.detach().numpy(), bins=20, range=(0, 1.0))
+    plt.show()
+
+
 if __name__ == "__main__":
-    # dataset = Planetoid(root='/tmp/Cora', name='Cora')
-    dataset = Planetoid(root='/tmp/Pubmed', name="Pubmed")
+    dataset = Planetoid(root='/tmp/Cora', name='Cora')
+    # dataset = Planetoid(root='/tmp/Pubmed', name="Pubmed")
     # dataset = Planetoid(root='/tmp/Citeseer', name='Citeseer')
     data = dataset[0]
     features = data.x
@@ -50,12 +64,14 @@ if __name__ == "__main__":
     train_mask = data.train_mask
     val_mask = data.val_mask
     test_mask = data.test_mask
-    lam1 = 1e-3
+    lam1 = 0
 
     # svc_experiment(graph, features, labels, train_mask, test_mask, 3)
     # neural_experiment(graph, features, labels, train_mask, test_mask, 3)
-    # E = learnProp_experiment(graph, features, labels, train_mask, val_mask, test_mask, lam1)
+    E = learnProp_experiment(graph, features, labels, train_mask, val_mask, test_mask, lam1, sim='mul')
     # improvedGCN(graph, features, labels, train_mask, val_mask, test_mask)
-    runGCN(data)
+    # runGCN(data)
 
     # draw_nx(graph, E, labels)
+    plt.hist(E.detach().numpy(), bins=20, range=(0, 1.0))
+    plt.show()
