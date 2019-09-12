8cf8a12cd9a8e5ff2f451d83c5f8198f4ea52ef5
marble0117 <guitarmarble0117@gmail.com>
2019-09-12 14:41:26 +0900

    implement function that eliminates important / unimportant edges

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
 import numpy as np
 import torch
 
+
+def accuracy(pred, labels):
+    _, indices = torch.max(pred, 1)
+    correct = (indices == labels).sum().item()
+    return correct / labels.size()[0]
+
+
 def devide_dataset(dataset, num_train_per_class, num_val, num_test):
     num_class = dataset.num_classes
     data = dataset[0]
@@ -19,12 +26,21 @@ def devide_dataset(dataset, num_train_per_class, num_val, num_test):
     test_mask = sample_mask(test_index, n)
     return train_mask, val_mask, test_mask
 
-def accuracy(pred, labels):
-    _, indices = torch.max(pred, 1)
-    correct = (indices == labels).sum().item()
-    return correct / labels.size()[0]
 
 def sample_mask(index, num_nodes):
     mask = torch.zeros((num_nodes, ), dtype=torch.uint8)
     mask[index] = 1
     return mask
+
+
+def eliminate_edges(edge_index, edge_importance, ratio=0.1, important=True):
+    # if important == True, eliminate the important edges from edge_index
+    n = edge_importance.size(0)
+    num_elim = int(n * ratio)
+    sorted_idx = torch.argsort(edge_importance[:, 0])
+    if important:
+        sorted_idx = sorted_idx[:n - num_elim]
+    else:
+        sorted_idx = sorted_idx[num_elim:]
+    edge_index = edge_index[:, sorted_idx]
+    return edge_index
