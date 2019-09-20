fde4e24c71107462e30966e9e4a0a8b11b0a87ba
marble0117 <guitarmarble0117@gmail.com>
2019-09-20 23:45:46 +0900

    add pytorch

--- exp_vis.py
+++ exp_vis.py
@@ -2,6 +2,7 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
+import torch
 from torch_geometric.datasets import Planetoid
 from torch_geometric.utils import add_self_loops
 
