19c19b05687a4c824992829dbeb44aa373d329cd
marble0117 <guitarmarble0117@gmail.com>
2019-10-08 18:37:32 +0900

    add Cent layer in the Net model

--- improvedGCN.py
+++ improvedGCN.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,21 +11,24 @@ from edge_processing import *
 
 
 class Net(MessagePassing):
-    def __init__(self, e_type, edge_index, features, nhid, nclass, sim):
+    def __init__(self, data, name, e_type, nhid, nclass, sim='cat'):
         super(Net, self).__init__()
         if e_type == 'sim':
-            self.edge_func = EdgeSimNet(edge_index, features, sim)
+            self.edge_func = EdgeSimNet(data.edge_index, data.x, sim)
         elif e_type == 'nn':
-            self.edge_func = EdgeCatNet(edge_index, features)
+            self.edge_func = EdgeCatNet(data.edge_index, data.x)
         elif e_type == 'conv':
-            self.edge_func = EdgeConvNet(edge_index, features, n_filt=2, d_out=4)
+            self.edge_func = EdgeConvNet(data.edge_index, data.x, n_filt=2, d_out=4)
+        elif e_type == 'cent':
+            cent_list = ["betweenness", "eigenvector", "cosine", "degree"]
+            self.edge_func = EdgeCentralityNet(data, name, cent_list)
         else:
             print("Invalid edge importance calculator:", e_type)
             exit(1)
-        self.edge_index = edge_index
-        self.fc1 = nn.Linear(features.size(1), nhid)
+        self.edge_index = data.edge_index
+        self.fc1 = nn.Linear(data.x.size(1), nhid)
         self.fc2 = nn.Linear(nhid, nclass)
-        self.dropout = nn.Dropout(p=0.5)
+        self.dropout = nn.Dropout(p=0.25)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
@@ -37,9 +41,10 @@ class Net(MessagePassing):
         x = self.dropout(x)
         x = self.fc2(x)
         x = self.propagate(self.edge_index, size=(x.size(0), x.size(0)), x=x, E=E, aggr='mean')
+        print(np.histogram(E.detach().numpy()))
 
         # prediction
-        return F.log_softmax(x, dim=1)
+        return F.log_softmax(x, dim=1), E
 
     def message(self, x_j, E):
         return x_j * E
@@ -51,19 +56,23 @@ def accuracy(pred, labels):
     return correct / labels.size()[0]
 
 
-def improvedGCN(edge_index, features, labels, train_mask, val_mask, test_mask, sim):
+def improvedGCN(data, name, e_type):
+    features = data.x
+    labels = data.y
+    train_mask = data.train_mask
+    val_mask = data.val_mask
+    test_mask = data.test_mask
 
     trainY = labels[train_mask == 1]
     valY = labels[val_mask == 1]
     testY = labels[test_mask == 1]
 
-    e_type = "conv"
-    net = Net(e_type, edge_index, features, 8, int(max(labels)) + 1, sim)
+    net = Net(data, name, e_type, 8, int(max(labels)) + 1)
     optimizer = torch.optim.Adam(net.parameters(), lr=0.01, weight_decay=5e-4)
     net.train()
-    for i in range(100):
+    for i in range(200):
         optimizer.zero_grad()
-        output = net(features)
+        output, _ = net(features)
         train_loss = F.nll_loss(output[train_mask == 1], trainY)
         val_loss = F.nll_loss(output[val_mask == 1], valY)
         val_acc = accuracy(output[val_mask == 1], valY)
@@ -72,9 +81,9 @@ def improvedGCN(edge_index, features, labels, train_mask, val_mask, test_mask, s
         loss.backward()
         optimizer.step()
     net.eval()
-    output = net(features)
+    output, E = net(features)
     acc_train = accuracy(output[train_mask == 1], trainY)
     print("train accuracy :", acc_train)
-    output = net(features)
     acc_test = accuracy(output[test_mask == 1], testY)
     print("test  accuracy :", acc_test)
+    return E
