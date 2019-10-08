e64f63524c0e28038f0e048d9e373c758a598ad1
marble0117 <guitarmarble0117@gmail.com>
2019-10-08 11:25:26 +0900

    fixed

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
 import torch
 
+
 def eliminate_interclass_edges(edge_index, labels):
     source, target = edge_index
     same_lbl = labels[source] == labels[target]
     edge_index = edge_index.T
-    edge_index = edge_index[same_lbl]
-    edge_index = edge_index.T
-    return edge_index
+    new_edge_index = edge_index[same_lbl]
+    new_edge_index = new_edge_index.T
+    return new_edge_index
